f0b5c338d45b01f8b5d5165fbccc4ee12a50fe75
Laura Turk <laura.turk@cognizant.com>
2017-10-12 13:22:15 -0600

    prettier image uploads add route for tags and build query for tags by type/count

--- dang-thats-delicious/models/Store.js
+++ dang-thats-delicious/models/Store.js
@@ -45,11 +45,23 @@ storeSchema.pre('save', async function(next) {
   this.slug = slug(this.name);
   const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
   const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
-  if(storesWithSlug.length) {
+  if (storesWithSlug.length) {
     this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
   }
   next();
 });
+
+storeSchema.statics.getTagsList = function() {
+  //using a proper function to have access to this functions are bound to model
+  return this.aggregate([
+    //first unwind (makes an instance of each store per tags)
+    { $unwind: '$tags' },
+    //second group unwound stores by tags and add a field of count +1 w/ each instance
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    //sort grouped descending
+    { $sort: { count: -1 } },
+  ]);
+};
 
 //main thing that is being exported (export default vs export)
 module.exports = mongoose.model('Store', storeSchema);
